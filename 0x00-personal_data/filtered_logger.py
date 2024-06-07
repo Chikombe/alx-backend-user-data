461e9b3987b140717510b0bd986a1399fc77c06e
Chikombe <chikombe.ecy@gmail.com>
2024-06-07 08:08:00 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,70 +1,95 @@
 #!/usr/bin/env python3
+""" Personal data.
 """
-This module provides a function to obfuscate specified fields in a log message.
-"""
-
-import re
 import logging
-from typing import List, Tuple
+import mysql.connector
+import re
+import os
+from typing import List
 
-# Define PII_FIELDS with fields considered as PII
-PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "credit_card")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Function to hide personal data.
+        Args:
+            fields: a list of strings.
+            redaction: a string representing by what the
+                       field will be obfuscated.
+            message: a string representing the log line.
+            separator: a string representing by which character
+                       is separating all fields in the log line.
+        Return:
+            The log message obfuscated.
     """
-    Obfuscates the specified fields in the log message.
-
-    :param fields: List of fields to obfuscate.
-    :param redaction: The string to replace the field values with.
-    :param message: The log message containing the fields.
-    :param separator: The character separating fields in the log message.
-    :return: The obfuscated log message.
-    """
-    return re.sub(
-        f'({"|".join(fields)})=.*?(?={separator}|$)',
-        lambda m: f'{m.group(1)}={redaction}',
-        message
-    )
+    for i in fields:
+        message = re.sub(r"{}=(.*?){}".format(i, separator),
+                         f'{i}={redaction}{separator}', message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """ Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Initializer.
+            Arg:
+                fields: a list of strings.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Format method.
+            Arg:
+                record: records of filter_datum function.
         """
-        Formats the log record, obfuscating specified fields.
-
-        :param record: The log record to format.
-        :return: The formatted log record as a string.
-        """
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
-                                  self.SEPARATOR)
-        return super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
-get_logger() -> logging.Logger:
+def get_logger() -> logging.Logger:
+    """ Function that stock log(logger), logger information gathering(handler)
+        and formatted forwarding(formatter).
     """
-    Creates and configures a logger named "user_data".
-
-    :return: Configured logger object.
-    """
-    logger = logging.getLogger("user_data")
+    logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
-
-    stream_handler = logging.StreamHandler()
+    handler = logging.StreamHandler()
     formatter = RedactingFormatter(fields=PII_FIELDS)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Function that connect to secure database.
+        Return:
+            A connector to the database.
+    """
+    return mysql.connector.connect(
+        host=os.getenv('PERSONAL_DATA_DB_HOST'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME'),
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD')
+    )
+
+
+def main():
+    """ Function that database connection using get_db and
+        retrieve all rows in the users table.
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+
+if __name__ == "__main__":
+    main()
